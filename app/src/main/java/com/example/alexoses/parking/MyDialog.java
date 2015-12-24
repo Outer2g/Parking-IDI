7faeef3b3ce5cae9661164842f0fe3cebf429d9e
Outer2g <aol.treballs@gmail.com>
2015-12-24 21:23:39 +0100

    Data passing done

--- app/src/main/java/com/example/alexoses/parking/MyDialog.java
+++ app/src/main/java/com/example/alexoses/parking/MyDialog.java
@@ -1,32 +1,47 @@
 package com.example.alexoses.parking;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Alex on 21/12/2015.
  */
 public class MyDialog extends android.support.v4.app.DialogFragment {
+    private EditText field;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         //Loading custom dialog Layout
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.placa_dialog,null));
+        View rootView = inflater.inflate(R.layout.placa_dialog,null);
+        builder.setView(rootView);
+        Bundle data = getArguments();
+        String s = data.getString("matricula");
+        field = (EditText) rootView.findViewById(R.id.matriculaIn);
+        TextView t = (TextView) rootView.findViewById(R.id.textView);
+        t.setText(s);
 
         //adding "ok" button
-        builder.setNeutralButton("Ok",new DialogInterface.OnClickListener() {
+        builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Intent intent = new Intent().putExtra("matriculaNova",field.getText());
+                getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK,intent);
+                dismiss();
             }
         });
         // Create the AlertDialog object and return it
         return builder.create();
     }
+
 
 }
--- app/src/main/java/com/example/alexoses/parking/ParkingActivity.java
+++ app/src/main/java/com/example/alexoses/parking/ParkingActivity.java
@@ -1,5 +1,7 @@
 package com.example.alexoses.parking;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -7,6 +9,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.example.alexoses.parking.domain.Parking;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Vector;
 
 
@@ -39,8 +46,31 @@ public class ParkingActivity extends ActionBarActivity {
 
         private Vector<Button> places;
         View rootView;
+        private Parking parking;
+        private final Fragment mainFragment = this;
+        private final int TOTAL_SPOTS = 12;
+        private final int MAT_REQUEST_CODE = 1;
         public PlaceholderFragment() {
         }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            switch (requestCode){
+                case MAT_REQUEST_CODE:
+                    if (resultCode == Activity.RESULT_OK){
+                        Bundle bundle = data.getExtras();
+                        String matricula = bundle.getString("matriculaNova");
+                        Calendar c = new GregorianCalendar(2015,3,28);
+                        try {
+                            parking.entersVehicle(matricula,c,2);
+                        } catch (Exception e) {
+                            Log.e("PARKING",e.getMessage());
+                        }
+                    }
+                    break;
+            }
+        }
+
         private Button createButton(){
             final Button but = new Button(getActivity());
             but.setBackgroundColor(Color.RED);
@@ -55,13 +85,25 @@ public class ParkingActivity extends ActionBarActivity {
                 but.setHeight(p.y / 12);
                 but.setWidth(p.x / 3);
             }
+
             but.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     ColorDrawable color = (ColorDrawable) but.getBackground();
                     if (color.getColor() == Color.RED) but.setBackgroundColor(Color.GREEN);
                     else but.setBackgroundColor(Color.RED);
-                    //new MyDialog().show(getFragmentManager(),"hi");
+
+                    MyDialog newDialog = new MyDialog();
+                    Bundle  args = new Bundle();
+                    try{
+                        args.putString("matricula", parking.getSpots().get(2).getNumberPlate());
+                    }
+                    catch (Exception e){
+                        args.putString("matricula","introdueixi nova matricula");
+                    }
+                    newDialog.setArguments(args);
+                    newDialog.setTargetFragment(mainFragment, MAT_REQUEST_CODE);
+                    newDialog.show(getActivity().getSupportFragmentManager(),"hi");
                 }
             });
             but.setText("");
@@ -82,7 +124,8 @@ public class ParkingActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             rootView = inflater.inflate(R.layout.fragment_parking, container, false);
-            places = new Vector<Button>(12);
+            places = new Vector<Button>(TOTAL_SPOTS);
+            parking = new Parking(TOTAL_SPOTS);
 
             //Agafo les dos files/columnes i les omplo amb botons
             LinearLayout fila1 = (LinearLayout) rootView.findViewById(R.id.fila1);
